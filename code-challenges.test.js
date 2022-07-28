b645cf46a938fd96d6c1cdff79009c2f1951ecf0
ninerboy81 <luis.ramos07@yahoo.com>
2022-07-28 16:55:00 -0700

    assessment complete...i hope

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -19,10 +19,46 @@ const people = [
   { name: "zaphod beeblebrox", occupation: "president of the galaxy" },
   { name: "arthur dent", occupation: "a radio employee" }
 ]
+
+describe ("capitalize", () => {
+  it("should capitalize the name", () => {
+    expect(capitalize(people)).toEqual([
+      "Ford Prefect is a hitchhiker",
+      "Zaphod Beeblebrox is president of the galaxy",
+      "Arthur Dent is a radio employee"
+    ]);
+  }
+  )
+}
+)
+
+
 // Expected output: ["Ford Prefect is a hitchhiker.", "Zaphod Beeblebrox is president of the galaxy.", "Arthur Dent is a radio employee."]
+
+// ReferenceError: capitalize is not defined
 
 
 // b) Create the function that makes the test pass.
+
+// pseudo code:
+// 1. create a function that takes in an array of objects
+// 2. use a .map method to iterate through the array
+// 3. create a variable called name and use .split to split the name into first and last name
+// 4. create a variable called firstName and use .charAt to get the first letter of the first name
+// 5. create a variable called lastName and use .charAt to get the first letter of the last name
+// 6. return the string
+
+
+const capitalize = (people) => {
+  return people.map(person => {
+    var name = person.name.split(" ");
+    var firstName = name[0].charAt(0).toUpperCase() + name[0].slice(1);
+    var lastName = name[1].charAt(0).toUpperCase() + name[1].slice(1);
+    return `${firstName} ${lastName} is ${person.occupation}`;
+  }
+  )
+}
+
 
 
 // --------------------2) Create a function that takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3.
@@ -34,8 +70,33 @@ const hodgepodge1 = [23, "Heyyyy!", 45, -10, 0, "Yo", false]
 const hodgepodge2 = [5, "Hola", 43, -34, "greetings", true]
 // Expected output: [ 2, 1, -1 ]
 
+describe ("remainder", () => {
+  it("should return the remainder of the number", () => {
+    expect(remainder(hodgepodge1)).toEqual([ 2, 0, -1, 0 ]);
+    expect(remainder(hodgepodge2)).toEqual([ 2, 1, -1 ]);
+  }
+  )
+}
+)
+
+// ReferenceError: remainder is not defined
 
 // b) Create the function that makes the test pass.
+
+
+// pseudo code:
+// 1. create a function that takes in a mixed data array
+// 2. use a .filter method to iterate through the array
+// 3. create a variable called value and use .typeof to get the type of the value
+// 4. create a variable called remainder and use .% to get the remainder of the value divided by 3
+// 5. return the remainder
+
+
+
+const remainder = (array) => {
+  return array.filter(value => typeof value === "number").map(value => value % 3);
+}
+
 
 
 // --------------------3) Create a function that takes in an array of numbers and returns the sum of all the numbers cubed.
@@ -47,5 +108,26 @@ const cubeAndSum1 = [2, 3, 4]
 const cubeAndSum2 = [0, 5, 10]
 // Expected output: 1125
 
+describe ("cubeAndSum", () => {
+  it("should return the sum of the cubes", () => {
+    expect(cubeAndSum(cubeAndSum1)).toEqual(99);
+    expect(cubeAndSum(cubeAndSum2)).toEqual(1125);
+  }
+  )
+}
+)
+
+// ReferenceError: cubeAndSum is not defined
 
 // b) Create the function that makes the test pass.
+
+// pseudo code:
+// 1. create a function that takes in an array of numbers
+// 2. use a .map method to iterate through the array
+// 3. create a variable called value and use .** to get the value cubed
+// 4. return the sum of the cubes
+  
+
+const cubeAndSum = (array) => {
+  return array.map(value => value ** 3).reduce((a, b) => a + b);
+}
--- rails-commenting.rb
+++ rails-commenting.rb
@@ -6,25 +6,25 @@
 
 # FILE: app/controller/blog_posts_controller.rb
 
-# ---1)
+# ---1) Using "<" means that the blogpostcontroller is a child of the ApplicationController. This means that the blogpostcontroller inherits all the methods and attributes of the ApplicationController.
 class BlogPostsController < ApplicationController
   def index
-    # ---2)
+    # ---2) @posts is an array of all the blog posts in the database. Using @ means that this is an instance variable. Using .all means that it is a class method.
     @posts = BlogPost.all
   end
 
-  # ---3)
+  # ---3) def show is a CRUD method. It is a GET method that displays the blog post.
   def show
     @post = BlogPost.find(params[:id])
   end
 
-  # ---4)
+  # ---4) def new is also a CRUD method. It is a GET method that displays the form for creating a new blog post.
   def new
     @post = BlogPost.new
   end
 
   def create
-    # ---5)
+    # ---5) @post is an instance variable. It is a new blog post. Using .create means that it is a class method. It creates a new blog post in the database.
     @post = BlogPost.create(blog_post_params)
     if @post.valid?
       redirect_to blog_post_path(@post)
@@ -34,13 +34,13 @@ class BlogPostsController < ApplicationController
   end
 
   def edit
-    # ---6)
+    # ---6) as above @post is an instance variable but this time using the .find method it finds the blog post in the database. Also with params[:id] it means that the id of the blog post is being passed in.
     @post = BlogPost.find(params[:id])
   end
 
   def update
     @post = BlogPost.find(params[:id])
-    # ---7)
+    # ---7) It is the blog post that is being updated. Using .update_attributes means that it is a class method. It updates the blog post in the database.
     @post.update(blog_post_params)
     if @post.valid?
       redirect_to blog_post_path(@post)
@@ -54,15 +54,15 @@ class BlogPostsController < ApplicationController
     if @post.destroy
       redirect_to blog_posts_path
     else
-      # ---8)
+      # ---8) redirect_to is a method that redirects to a different page. blog_posts_path is a method that returns the path to the blog posts page.
       redirect_to blog_post_path(@post)
     end
   end
 
-  # ---9)
+  # ---9) private is a method that is used to define methods that are only accessible within the class. it is to make sure that no one else can access the methods
   private
   def blog_post_params
-    # ---10)
+    # ---10) params.require is a method that is used to require that a certain parameter is passed in. It is to make sure that the user is passing in the correct parameters. .permit is to make sure that certain parameters are passed in.
     params.require(:blog_post).permit(:title, :content)
   end
 end
